d460371522cc41e581653f30d3f2f2e8c40bd186
Brenton Annan <brentonannan@brentonannan.com>
2016-06-03 18:50:59 +1000

    Release 0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule UeberauthAuthy.Mixfile do
 
   def project do
     [app: :ueberauth_authy,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
